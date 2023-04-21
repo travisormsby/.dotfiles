306466143fc0fca3483088bb7be37e1f0da81a03
Travis Ormsby <travis.ormsby@gmail.com>
2023-04-20 20:21:21 -0500

    update sync script

--- sync.sh
+++ sync.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
 # Back up existing files and create symlink to repo files
-# .bashrc first
-mv --backup=numbered ~/.bashrc ~/.bashrc.back
-ln -s $(pwd)/.bashrc ~/.bashrc
-
-# then all folders in .config
-for f in .config/*; do
+for f in .config/* .bashrc; do
     mv --backup=numbered ~/$f ~/$f.back
     ln -s $(pwd)/$f ~/$f
 done
